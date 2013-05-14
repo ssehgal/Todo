843d1f64a84da206ce8170a220621ba3aff59d65
Saloni Sehgal <ssehgal@tavisca.com>
2013-05-14 16:27:07 +0530

    made changes to the css and javascript for the visibility of add list item button

--- Todo.js
+++ Todo.js
@@ -2,12 +2,21 @@ $(function(){
 	$("#btnAddList").click(function()
 	{
 		var value=$("#appendedInputButtons1").val();
-		$("#listTable").append("<tr><td>"+value+"</td><tr>");
+		$("#listTable").append("<tr><td class='nameList'>"+value+"</td><tr>");
 		$("#appendedInputButtons1").val("");
+		$("#listItemInput").removeClass("invis");
+		bindClick();
 	})
+	function bindClick(){
+	$(".nameList").unbind("click").click(function(){
+		$(".nameList").removeClass("selected");
+		$(this).addClass("selected");
 	
+	})
+	}
 	$("#btnAddItem").click(function()
 	{
+		
 		var value=$("#appendedInputButtons2").val();
 		$("#itemTable").append("<tr><td>"+value+"</td><tr>");
 		$("#appendedInputButtons2").val("");
